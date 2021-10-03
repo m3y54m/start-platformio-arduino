b70f2753fb9605ae17bb8ec79ff8658313c5ad09
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-03 15:29:18 +0330

    Simple serial port counter

--- arduino_start/src/main.cpp
+++ arduino_start/src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
 
+long int counter;
+
+// put your setup code here, to run once:
 void setup() {
-  // put your setup code here, to run once:
+
+  counter = 0;
+  
   Serial.begin(9600);
-  Serial.println("Hello World!");
+  Serial.println("[ Serial Communication Started ]");
 }
 
+// put your main code here, to run repeatedly:
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Increment the counter by 1
+  counter += 1;
+
+  // Send numbers to serial port
+  Serial.print("#");
+  Serial.println(counter);
+
+  // Wait for 1 second
+  delay(1000);
 }
